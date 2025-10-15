a13b3ff924f79fd9db8d17f8610f9e2ebbdbbed1
faizzaidi23 <786faizzaidi@gmail.com>
2025-10-15 23:24:41 +0530

    changed the gradle and the toml file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,19 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.ksp)
+    // add the kotlinx serialization plugin to handle the json data
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.21"
 }
 
 android {
-    namespace = "com.example.noteflowproduction"
-    compileSdk = 35
+    namespace = "com.example.expensecalculator"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.noteflowproduction"
+        applicationId = "com.example.expensecalculator"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -28,27 +30,48 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
 }
 
 dependencies {
-
+    // AndroidX & Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
+    // Jetpack Compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    // Room Database
+    implementation(libs.bundles.androidx.room)
+    ksp(libs.androidx.room.compiler)
+
+    //----Additions for networking
+    implementation("com.squareup.retrofit2:retrofit:2.9.0") // The core library for making network requests to your backend
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0") // The library for converting Kotlin objects to and from JSON.
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0") // A converter that allows retrofit to use kotlinx Serialization
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0") // A utility to log network request and response details, which is very helpful for debugging
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/noteflowproduction/1_jsut.kt
+++
@@ -1,9 +0,0 @@
-package com.example.noteflowproduction
-
-import androidx.compose.runtime.Composable
-
-
-@Composable
-fun addTwoNumbers(){
-    val a=5
-}
