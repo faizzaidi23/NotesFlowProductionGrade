87728367bd84039bd7ba12bc6e43cbc3195b41e0
faizzaidi23 <786faizzaidi@gmail.com>
2025-10-16 23:40:22 +0530

    feat:built the FolderDao.kt file

---
+++ app/src/main/java/com/example/noteflowproduction/Database/3_FolderDao.kt
@@ -0,0 +1,25 @@
+package com.example.noteflowproduction.Database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FolderDao{
+
+
+    //To show  all the folders
+    @Query("select * from folders order by createdDate")
+    fun showAllFolders():Flow<List<Folder>>
+
+    @Insert
+    suspend fun addFolder(folder: Folder)
+
+    @Insert
+    suspend fun updateFolder(folder: Folder)
+
+    @Delete
+    suspend fun deleteFolder(folder: Folder)
+}
