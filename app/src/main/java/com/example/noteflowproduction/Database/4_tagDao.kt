26bfe446424c1edc6cb4e6c5ff3be2582c073caf
faizzaidi23 <786faizzaidi@gmail.com>
2025-10-17 22:53:36 +0530

    feat:completed the tagDao.kt file

--- app/src/main/java/com/example/noteflowproduction/Database/4_tagDao.kt
+++ app/src/main/java/com/example/noteflowproduction/Database/4_tagDao.kt
@@ -1,2 +1,19 @@
 package com.example.noteflowproduction.Database
 
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+@Dao
+interface TagDao{
+
+    //To get all tags
+    @Query("select * from tags")
+    fun getAllTags():Flow<List<Tag>>
+
+    @Query("""
+        select tags.* from tags
+        inner join note_tag_cross_ref on tags.tagId = note_tag_cross_ref.tagId
+        where note_tag_cross_ref.id = :noteId
+    """)
+    fun getTagsForNote(noteId: Int):Flow<List<Tag>>
+}
