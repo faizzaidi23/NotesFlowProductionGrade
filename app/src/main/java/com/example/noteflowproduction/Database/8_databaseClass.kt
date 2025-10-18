b0071c4de1b08c678097d80562fd69cfe566d116
faizzaidi23 <786faizzaidi@gmail.com>
2025-10-18 12:53:37 +0530

    made the database file

--- app/src/main/java/com/example/noteflowproduction/Database/8_databaseClass.kt
+++ app/src/main/java/com/example/noteflowproduction/Database/8_databaseClass.kt
@@ -1,2 +1,27 @@
 package com.example.noteflowproduction.Database
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [
+        Folder::class,
+        Note::class,
+        Tag::class,
+        NoteTagCrossRef::class,
+        Media::class,
+        Reminder::class
+    ],
+    version = 1,
+    exportSchema=false
+)
+@TypeConverters(Converters::class)
+abstract class AppDatabase: RoomDatabase(){
+    abstract fun noteDao(): NoteDao
+    abstract fun folderDao(): FolderDao
+    abstract fun tagDao(): TagDao
+    abstract fun reminderDao(): ReminderDao
+    abstract fun noteTagCrossRefDao(): NoteTagCrossRefDao
+}
