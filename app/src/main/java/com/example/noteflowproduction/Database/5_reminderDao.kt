af61afb08a54a38d2a6eb1a1e5fe7db2b251c0bb
faizzaidi23 <786faizzaidi@gmail.com>
2025-10-18 11:53:59 +0530

    feat:completed the reminderDao.kt file

--- app/src/main/java/com/example/noteflowproduction/Database/5_reminderDao.kt
+++ app/src/main/java/com/example/noteflowproduction/Database/5_reminderDao.kt
@@ -23,4 +23,16 @@ interface ReminderDao{
     @Query("select * from reminders")
     fun showAllReminders():Flow<List<Reminder>>
 
+    //reminders for a specific note
+    @Query("select * from reminders where noteId = :noteId")
+    fun getRemindersForNote(noteId: Int):Flow<List<Reminder>>
+
+    @Query("""
+        select distinct r.* from reminders r
+        inner join notes n on r.noteId = n.id
+        inner join note_tag_cross_ref x on x.id = n.id
+        where x.tagId = :tagId
+    """)
+    fun getRemindersForTag(tagId: Int):Flow<List<Reminder>>
+
 }
