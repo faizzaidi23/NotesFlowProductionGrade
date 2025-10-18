68a9d6f8d86e666085781bf1dea0dbd554aa7d74
faizzaidi23 <786faizzaidi@gmail.com>
2025-10-18 14:42:22 +0530

    feat:made the HiltAndroidApp annotation for the NotesFlowApplication class

---
+++ app/src/main/java/com/example/noteflowproduction/Hilt/9_applicationClass.kt
@@ -0,0 +1,7 @@
+package com.example.noteflowproduction.Hilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NotesFlowApplication(): Application()
