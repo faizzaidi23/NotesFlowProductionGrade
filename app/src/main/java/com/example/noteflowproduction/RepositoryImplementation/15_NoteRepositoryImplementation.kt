4e3e63d8533eb190258e156fc7ae4c0899bca5c1
faizzaidi23 <786faizzaidi@gmail.com>
2025-10-19 19:47:20 +0530

    feat:updated the NoteRepositoryImplementation.kt

--- app/src/main/java/com/example/noteflowproduction/RepositoryImplementation/15_NoteRepositoryImplementation.kt
+++ app/src/main/java/com/example/noteflowproduction/RepositoryImplementation/15_NoteRepositoryImplementation.kt
@@ -2,39 +2,40 @@ package com.example.noteflowproduction.RepositoryImplementation
 
 import com.example.noteflowproduction.Database.Note
 import com.example.noteflowproduction.Database.NoteDao
+import com.example.noteflowproduction.Repositories.NoteRepository
 import kotlinx.coroutines.flow.Flow
 
-class NoteRepositoryImplementation(private val noteDao:NoteDao){
+class NoteRepositoryImplementation(private val noteDao:NoteDao): NoteRepository{
 
-    fun getAllNotes():Flow<List<Note>> = noteDao.showAllNotes()
+    override fun getAllNotes():Flow<List<Note>> = noteDao.showAllNotes()
 
     //Adding a new note
-    suspend fun addNote(note: Note){
+    override suspend fun addNote(note: Note){
         noteDao.addNote(note=note)
     }
 
     //Updating a note
-    suspend fun updateNote(note: Note){
+    override suspend fun updateNote(note: Note){
         noteDao.updateNote(note=note)
     }
 
     //Deleting a note
-    suspend fun deleteNote(note: Note){
+    override suspend fun deleteNote(note: Note){
         noteDao.deleteNote(note=note)
     }
 
     //Getting a new note by Id
-    suspend fun getNoteById(id: Int){
-        noteDao.getNoteById(id=id)
+    override suspend fun getNoteById(id: Int):Note?{
+        return noteDao.getNoteById(id=id)
     }
 
     //Getting a note by folder id
-    fun getNoteByFolderId(folderId: Int):Flow<List<Note>>{
+    override fun getNotesByFolder(folderId: Int):Flow<List<Note>>{
         return noteDao.getNoteByFolderId(folderId = folderId)
     }
 
     //Getting all the notes by a tag
-    fun getNotesByTag(tagId: Int):Flow<List<Note>>{
+    override fun getNotesByTag(tagId: Int):Flow<List<Note>>{
         return noteDao.getNotesForTag(tagId = tagId)
     }
 
