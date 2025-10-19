77e95513f3a76fa7a42d9a92e3b6080c99640e11
faizzaidi23 <786faizzaidi@gmail.com>
2025-10-19 19:59:59 +0530

    feat:updated and cleared the glitches from reminderRepositoryImplementation and the tagRepositoryImplementation

--- app/src/main/java/com/example/noteflowproduction/RepositoryImplementation/17_RemainderRepositoryImplementation.kt
+++ app/src/main/java/com/example/noteflowproduction/RepositoryImplementation/17_RemainderRepositoryImplementation.kt
@@ -3,37 +3,38 @@ package com.example.noteflowproduction.RepositoryImplementation
 import com.example.noteflowproduction.Database.Note
 import com.example.noteflowproduction.Database.Reminder
 import com.example.noteflowproduction.Database.ReminderDao
+import com.example.noteflowproduction.Repositories.ReminderRepository
 import kotlinx.coroutines.flow.Flow
 
-class ReminderRepository(private val reminderDao:ReminderDao){
+class ReminderRepositoryImplementation(private val reminderDao: ReminderDao) : ReminderRepository {
 
     //To get all the reminders
-    fun getAllReminders():Flow<List<Reminder>>{
+    override fun getAllReminders(): Flow<List<Reminder>> {
         return reminderDao.showAllReminders()
     }
 
     //Add a reminder
-    suspend fun addReminder(reminder: Reminder){
+    override suspend fun addReminder(reminder: Reminder) {
         reminderDao.addReminder(reminder = reminder)
     }
 
     //update a reminder
-    suspend fun updateReminder(reminder: Reminder){
-        reminderDao.updateReminder(reminder=reminder)
+    override suspend fun updateReminder(reminder: Reminder) {
+        reminderDao.updateReminder(reminder = reminder)
     }
 
     //delete a reminder
-    suspend fun deleteReminder(reminder:Reminder){
-        reminderDao.deleteReminder(reminder=reminder)
+    override suspend fun deleteReminder(reminder: Reminder) {
+        reminderDao.deleteReminder(reminder = reminder)
     }
 
     //get reminders for note
-    fun getReminderForNote(noteId:Int):Flow<List<Reminder>>{
-        return reminderDao.getRemindersForNote(noteId=noteId)
+    override fun getRemindersForNote(noteId: Int): Flow<List<Reminder>> {
+        return reminderDao.getRemindersForNote(noteId = noteId)
     }
 
     //get reminders for tag
-    fun getReminderForTag(tagId: Int):Flow<List<Reminder>>{
+    override fun getRemindersForTag(tagId: Int): Flow<List<Reminder>> {
         return reminderDao.getRemindersForTag(tagId = tagId)
     }
 }
--- app/src/main/java/com/example/noteflowproduction/RepositoryImplementation/18_tagRepositoryImplementation.kt
+++ app/src/main/java/com/example/noteflowproduction/RepositoryImplementation/18_tagRepositoryImplementation.kt
@@ -1,33 +1,41 @@
 package com.example.noteflowproduction.RepositoryImplementation
 
+import com.example.noteflowproduction.Database.Note
+import com.example.noteflowproduction.Database.NoteDao
 import com.example.noteflowproduction.Database.Tag
 import com.example.noteflowproduction.Database.TagDao
+import com.example.noteflowproduction.Repositories.tagRepository
 import kotlinx.coroutines.flow.Flow
 
-class TagRepository(private val tagDao: TagDao){
+class TagRepositoryImplementation(private val tagDao: TagDao, private val noteDao: NoteDao): tagRepository {
 
     //Get all tags
-    fun getAllTags():Flow<List<Tag>>{
+    override fun getAllTags():Flow<List<Tag>>{
         return tagDao.getAllTags()
     }
 
     //Add a Tag
-    suspend fun addTag(tag: Tag){
-        tagDao.addTag(tag=tag)
+    override suspend fun addTag(tagId: Tag){
+        tagDao.addTag(tag=tagId)
     }
 
     //update a tag
-    suspend fun updateTag(tag: Tag){
-        tagDao.updateTag(tag=tag)
+    override suspend fun updateTag(tagId: Tag){
+        tagDao.updateTag(tag=tagId)
     }
 
     //delete a tag
-    suspend fun deleteTag(tag: Tag){
-        tagDao.deleteTag(tag=tag)
+    override suspend fun deleteTag(tagId: Tag){
+        tagDao.deleteTag(tag=tagId)
+    }
+
+    //get all notes for a tag
+    override fun getAllNotesForTag(tagId: Int):Flow<List<Note>>{
+        return noteDao.getNotesForTag(tagId = tagId)
     }
 
     //get tags for note
-    fun getTagsForNote(noteId: Int){
-        tagDao.getTagsForNote(noteId = noteId)
+    override fun getAllTagsForNote(noteId: Int):Flow<List<Tag>>{
+        return tagDao.getTagsForNote(noteId = noteId)
     }
 }
