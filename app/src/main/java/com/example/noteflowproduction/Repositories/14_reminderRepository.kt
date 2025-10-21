681d125cab248c8517104ce222ed2e4f36c6de43
faizzaidi23 <786faizzaidi@gmail.com>
2025-10-21 10:56:29 +0530

    feat:updated the reminderRepository

--- app/src/main/java/com/example/noteflowproduction/Repositories/14_reminderRepository.kt
+++ app/src/main/java/com/example/noteflowproduction/Repositories/14_reminderRepository.kt
@@ -5,22 +5,22 @@ import kotlinx.coroutines.flow.Flow
 
 interface ReminderRepository{
 
-    //To get all the reminders
+
     fun getAllReminders():Flow<List<Reminder>>
 
-    //To get all the reminders for a particular note
+
     fun getRemindersForNote(noteId: Int):Flow<List<Reminder>>
 
-    //To get all the reminders for a particular tag
+
     fun getRemindersForTag(tagId: Int):Flow<List<Reminder>>
 
-    //To add a reminder
+
     suspend fun addReminder(reminder: Reminder)
 
-    //To update a reminder
+
     suspend fun updateReminder(reminder: Reminder)
 
-    //To delete a reminder
+
     suspend fun deleteReminder(reminder: Reminder)
 
 }
