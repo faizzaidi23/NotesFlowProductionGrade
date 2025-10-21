e4c12633902197f491897df749c50b439ba88e57
faizzaidi23 <786faizzaidi@gmail.com>
2025-10-21 11:14:31 +0530

    feat: made the screen definition screen

--- app/src/main/java/com/example/noteflowproduction/NavigationSetup/23_screensDefinition.kt
+++ app/src/main/java/com/example/noteflowproduction/NavigationSetup/23_screensDefinition.kt
@@ -1,2 +1,8 @@
 package com.example.noteflowproduction.NavigationSetup
 
+sealed class Screen(val route: String){
+    object HomeScreen : Screen("home")
+    object FolderScreen:Screen("folder")
+    object ReminderScreen:Screen("reminder")
+    object TagScreen:Screen("tagScreen")
+}
