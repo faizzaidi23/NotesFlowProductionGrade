8a22df2497a85916df241df9e2b72de3272445d4
faizzaidi23 <786faizzaidi@gmail.com>
2025-10-22 23:04:57 +0530

    feat:Initialized the ui part of the project with the notesHomeScreen

--- app/src/main/java/com/example/noteflowproduction/UserInterface/25_HomeScreen.kt
+++ app/src/main/java/com/example/noteflowproduction/UserInterface/25_HomeScreen.kt
@@ -1,9 +1,29 @@
 package com.example.noteflowproduction.UserInterface
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController){
+    Scaffold(
+        modifier=Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title={Text(text="NotesFlow",fontWeight=FontWeight.ExtraBold,fontSize=36.sp)},
+                modifier=Modifier.fillMaxSize(),
+                navigationIcon = R.drawad
 
+            )
+        }
+    ) {  }
 }
